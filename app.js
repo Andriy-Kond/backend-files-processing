6554885883798d05c335d9e81b83f2e46cec052d
Andrii <andrii.kondratovich@gmail.com>
2025-01-12 10:32:22 +0200

    add comm

--- app.js
+++ app.js
@@ -19,27 +19,28 @@ app.use(express.static("public")); // If you will receive the GET request to a f
 const books = []; // instead of a external server
 
 // Settings object for middleware multer:
-// const upload = multer({ dest: "uploads/" });
 const multerConfig = multer.diskStorage({
-  destination: tempDir, // The folder to which the file has been saved
+  destination: tempDir, // The folder to which the file has been saved after upload (temp dir)
 
   // The name of the file within the destination:
   filename: (req, file, cb) => {
     // This fn can save file with a different name (not the name it was received with)
     // file - it is the file received and save in memory but not save at storage
     cb(null, file.originalname); // will save with original name file (In this example the "filename" field is not needs)
-    // null - there you can send an error if something goes wrong: cb(new Error('I don't have a clue!'))
+    // null - there you can send an error if something goes wrong. For example: cb(new Error('I don't have a clue!'))
   },
 });
 
+// const upload = multer({ dest: "uploads/" });
 const upload = multer({ storage: multerConfig }); // "dest" or "storage": Where to store the files (tempDir + name specified in cb)
+
 //~ get books from "server"
 app.get("/api/books", (req, res) => {
   res.json(books);
 });
 
 //~ add books to the "server"
-// //$ add one file in one field AND many files in other field
+// //$ add one file in one field!AND! many files in other field
 // // The "multer" cannot to use .single() and .array() in one request. For this you must use method .fields()
 // const cpUpload = upload.fields([
 //   { name: "cover", maxCount: 1 }, // field fo one file
@@ -58,13 +59,15 @@ const errCb = err => {
   console.log("Rename complete!");
 };
 
-//$ add one file in one field OR many files in one field
+//$ add one file in one field !OR! many files in one field
 const uploadDir = path.join(__dirname, "public", "books");
 app.post(
   "/api/books",
-  upload.single("cover"),
-  // or:
+  //$ or:
+  upload.single("cover"), // for one file
+  //$ or:
   // upload.array("photos", 3), // for multiple files
+
   async (req, res) => {
     // req.file is info about one file from .single() method
     // console.log("req.file:::", req.file);
